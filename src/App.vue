e6a8c9a319d479753aa6a7b5cdee509db1398182
adityabcgi <aditya.borikar@claimgenius.com>
2024-09-21 12:13:05 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex justify-center items-center">
+    <div class="card bg-base-100 w-96 shadow-xl">
+      <figure>
+        <img src="./assets/cover-image-edited.jpg" alt="Shoes" />
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title">My Portfolio</h2>
+        <p>Initial commit</p>
+        <div class="card-actions justify-end">
+          <button class="btn btn-success">Hurray!</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
